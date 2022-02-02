0b01330b764accef36f20c5667258388b3619876
Sumin Oh <ohsuminp@gmail.com>
2022-02-02 15:55:06 +0900

    Alert now returns winner instead of undefined.

--- script.js
+++ script.js
@@ -7,19 +7,37 @@ var computerSelection = ['Rock', 'Paper', 'Scissors'];
 console.log(computerPlay(computerSelection)); 
 //console is used to test whether function works as intended 
 
-var playerSelection = prompt("Enter 'Rock', 'Paper', or 'Scissors'", "");
+var playerPrompt = prompt("Enter 'Rock', 'Paper', or 'Scissors'");
+
+function capitalize(playerPrompt) {
+    return playerPrompt.charAt(0).toUpperCase() + playerPrompt.slice(1).toLowerCase();
+}
+
+var playerSelection = capitalize(playerPrompt);
+
+console.log(capitalize(playerSelection));
+//console is used to test whether function works as intended 
 
 function playRound(playerSelection, computerSelection) {
-    if ((playerSelection != null) && (playerSelection == 'Rock' && computerSelection == 'Scissors' || playerSelection == 'Scissors' && computerSelection == 'Paper' || playerSelection == 'Paper' && computerSelection == 'Rock')) {
-        console.log("You win! " + playerSelection + " beats " + computerSelection + ".");
+    let result;
+    if ((playerSelection == 'Rock' && computerPlay(computerSelection) == 'Scissors' || playerSelection == 'Scissors' && computerPlay(computerSelection) == 'Paper' || playerSelection == 'Paper' && computerPlay(computerSelection) == 'Rock')) {
+        result = "You win! " + playerSelection + " beats " + computerPlay(computerSelection) + ".";
     }    
-    else if ((playerSelection != null) && (playerSelection == 'Rock' && computerSelection == 'Paper' || playerSelection == 'Scissors' && computerSelection == 'Rock' || playerSelection == 'Paper' && computerSelection == 'Scissors')) {
-        console.log("Sorry, you lose! " + computerSelection + " beats " + playerSelection + ".");
+    else if ((playerSelection == 'Rock' && computerPlay(computerSelection) == 'Paper' || playerSelection == 'Scissors' && computerPlay(computerSelection) == 'Rock' || playerSelection == 'Paper' && computerPlay(computerSelection) == 'Scissors')) {
+        result = "Sorry, you lose! " + computerPlay(computerSelection) + " beats " + playerSelection + ".";
     }
-    else if ((playerSelection != null) && (playerSelection == 'Rock' && computerSelection == 'Rock' || playerSelection == 'Scissors' && computerSelection == 'Scissors' || playerSelection == 'Paper' && computerSelection == 'Paper')) {
-        console.log("It's a tie! You both played " + computerSelection + ".");
+    else if ((playerSelection == 'Rock' && computerPlay(computerSelection) == 'Rock' || playerSelection == 'Scissors' && computerPlay(computerSelection) == 'Scissors' || playerSelection == 'Paper' && computerPlay(computerSelection) == 'Paper')) {
+        result = "It's a tie! You both played " + computerPlay(computerSelection) + ".";
     }
+    return result;
 }
+
+alert(playRound(playerSelection, computerSelection));
+
+
+
+
+
 
 
 
