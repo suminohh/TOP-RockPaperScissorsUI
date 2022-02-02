d8b6f7f0eced8bf557fe2336412cbd348910eb5f
Sumin Oh <ohsuminp@gmail.com>
2022-02-02 13:54:21 +0900

    Push prompt when it did not before

--- script.js
+++ script.js
@@ -2,20 +2,24 @@ function computerPlay(computerSelection)
 {
  return computerSelection[Math.floor(Math.random()*computerSelection.length)];   
 }
-var compterSelection = ['Rock', 'Paper', 'Scissors'];
+var computerSelection = ['Rock', 'Paper', 'Scissors'];
 
-//console.log(computerPlay(hand)); 
+console.log(computerPlay(computerSelection)); 
 //console is used to test whether function works as intended 
 
+var playerSelection = prompt("Enter 'Rock', 'Paper', or 'Scissors'", "");
+
 function playRound(playerSelection, computerSelection) {
-    var playerSelection = prompt("Enter 'Rock', 'Paper', or 'Scissors'", "");
     if ((playerSelection != null) && (playerSelection == 'Rock' && computerSelection == 'Scissors' || playerSelection == 'Scissors' && computerSelection == 'Paper' || playerSelection == 'Paper' && computerSelection == 'Rock')) {
-        return "You win! " + playerSelection + " beats " + computerSelection + ".";
+        console.log("You win! " + playerSelection + " beats " + computerSelection + ".");
     }    
     else if ((playerSelection != null) && (playerSelection == 'Rock' && computerSelection == 'Paper' || playerSelection == 'Scissors' && computerSelection == 'Rock' || playerSelection == 'Paper' && computerSelection == 'Scissors')) {
-        return "Sorry, you lose! " + computerSelection + " beats " + playerSelection + ".";
+        console.log("Sorry, you lose! " + computerSelection + " beats " + playerSelection + ".");
     }
     else if ((playerSelection != null) && (playerSelection == 'Rock' && computerSelection == 'Rock' || playerSelection == 'Scissors' && computerSelection == 'Scissors' || playerSelection == 'Paper' && computerSelection == 'Paper')) {
-        return "It's a tie! You both played " + computerSelection + ".";
+        console.log("It's a tie! You both played " + computerSelection + ".");
     }
 }
+
+
+
